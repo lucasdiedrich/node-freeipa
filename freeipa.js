7efbc4126d772ccc9749d0abe1fcfb1769e36c75
Lucas Guilherme Diedrich <lucas.diedrich@unila.edu.br>
2016-07-15 15:30:41 -0300

    Auto-generating client class, Fixes #2

--- freeipa.js
+++ freeipa.js
@@ -1,23 +1,34 @@
 'use strict';
 
 var	Config 	= require('./lib/Config'),
-	Req 	= require('./lib/RequestBuilder');
+	Client  = require('./lib/Client');
+
+const METADATA = 'json_metadata';
+var client = null;
+var self = this;
 
 /*
 	Just an wrapper for call function without the path param. 
 */
 function call(method, args, options)
 {
+	var Req = require('./lib/RequestBuilder');
+
 	return Req.build(method, args || [""], options || {});
 }
 
 /*
-	Init config class, create first session, rebuild class with new methods;
+	Init config class, rebuild client class with new methods;
 */
 function configure(_options)
 {
 	Config.init(_options);
+
+	new Client(call).then(function(client)
+	{
+		module.exports.c = client;
+	});
 }
 
 module.exports.configure = configure;
-module.exports.call 	 = call;
+module.exports.call = call;
