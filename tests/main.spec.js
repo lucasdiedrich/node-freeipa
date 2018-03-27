27059c412dc4d2ffddd457d33057a955e711d63d
Lucas Guilherme Diedrich <lucas.diedrich@unila.edu.br>
2018-03-27 15:36:49 -0300

    Fix test suite

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,9 +1,11 @@
+// import { expect } from 'chai';
+let { expect } = require('chai');
 
 describe('Main', () => {
-	describe('Method A', () => {
-
-		it('it should do something', () => {
-			console.log("teste")
-		})
-	})
-})
+  describe('Method A', () => {
+    it('it should do something', () => {
+      expect(true).to.be.true;
+      console.log('teste');
+    });
+  });
+});
