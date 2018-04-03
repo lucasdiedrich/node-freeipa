5d33c7d755ff1542a606f6012e76572cf041c65f
Lucas Guilherme Diedrich <lucas.diedrich@unila.edu.br>
2018-04-03 17:49:33 -0300

    Changed cache folder to .tmp Created request tests Changed session class

--- test/unit/client.spec.js
+++ test/unit/client.spec.js
@@ -1,14 +1,24 @@
 const ipa = require('../../freeipa');
+const sinon = require('sinon');
 
-describe('consumer', () => {
+const userFind = sinon.spy(ipa, 'user_find');
+
+describe('Client tests', () => {
   // const timeout = 5000;
 
   it('should the new client be defined', () => {
     expect(ipa.user_find).to.exist;
   });
 
-  it('should call user_find redirect to new client', () => {
+  it('should call exists and be an function', () => {
     expect(ipa.call).to.be.an.instanceof(Function);
+  });
+
+  it('should call user_find redirect to new client', () => {
+    ipa.configure(global.fx.config);
+
+    ipa.call('user_find');
+    expect(userFind.called).to.be.true;
   });
 
   it('should the client ipa.c be null', () => {
@@ -19,6 +29,14 @@ describe('consumer', () => {
     global.fx.config.configure_client = true;
     ipa.configure(global.fx.config);
     expect(ipa.c).to.not.be.null;
+  });
+
+  it('should the client ipa.c redirect to new client2', () => {
+    global.fx.config.configure_client = true;
+    ipa.configure(global.fx.config);
+
+    ipa.c.user_find();
+    expect(userFind.callCount).to.be.at.least(2);
   });
 
   it('should the client ipa.c redirect to new client', () => {
--- test/unit/session.spec.js
+++ test/unit/session.spec.js
@@ -1,55 +1,68 @@
 
 const Session = require('../../lib/Session');
+const ipa = require('../../freeipa');
 
 const fs = require('fs');
 const path = require('path');
 
-const CACHE_FOLDER = path.join(__dirname, '../../', 'cache');
+const CACHE_FOLDER = path.join(__dirname, '../../', '.tmp');
 const CACHE_PATH = `${CACHE_FOLDER}/cookie.json`;
 
 const FAKE_DATE = new Date().getTime();
 const FAKE_TOKEN = `ipa_session=MagBearerToken=ve9ft7if3SCZm6kJM%2bVzytBkEDR61TDitzc4W90PPe%2bhaGNl%2fw5UMwEdCiaxCz54E56N0Y9nDvt2FmTDMyXGmwmdPgyr7gvIh0i8ewKsoQkYE8T%2bR0Si63bS5z1UZ0fzKBtasWtsXbUd2m3hG%2fM4Fs29LM%2bzL7wyzRlk8wl9ca8Anvj5Dwe8N3cJXNK77kBs7OZc7fja5BDN7PlKRGmU%2fg%3d%3d;path=/ipa;httponly;expires=${FAKE_DATE};secure;`;
 const FAKE_TOKEN45 = 'ipa_session=MagBearerToken=ve9ft7if3SCZm6kJM%2bVzytBkEDR61TDitzc4W90PPe%2bhaGNl%2fw5UMwEdCiaxCz54E56N0Y9nDvt2FmTDMyXGmwmdPgyr7gvIh0i8ewKsoQkYE8T%2bR0Si63bS5z1UZ0fzKBtasWtsXbUd2m3hG%2fM4Fs29LM%2bzL7wyzRlk8wl9ca8Anvj5Dwe8N3cJXNK77kBs7OZc7fja5BDN7PlKRGmU%2fg%3d%3d;path=/ipa;httponly;secure;';
+const FUTURE_FAKE_DATE = new Date((new Date()).getTime() + (10 * 86400000)).getTime();
+const FUTURE_FAKE_TOKEN = `ipa_session=MagBearerToken=ve9ft7if3SCZm6kJM%2bVzytBkEDR61TDitzc4W90PPe%2bhaGNl%2fw5UMwEdCiaxCz54E56N0Y9nDvt2FmTDMyXGmwmdPgyr7gvIh0i8ewKsoQkYE8T%2bR0Si63bS5z1UZ0fzKBtasWtsXbUd2m3hG%2fM4Fs29LM%2bzL7wyzRlk8wl9ca8Anvj5Dwe8N3cJXNK77kBs7OZc7fja5BDN7PlKRGmU%2fg%3d%3d;path=/ipa;httponly;expires=${FUTURE_FAKE_DATE};secure;`;
 
 describe('Session Token tests', () => {
   let session = null;
 
   beforeEach(() => {
+    session = null;
     try {
       fs.unlinkSync(CACHE_PATH);
       fs.rmdirSync(CACHE_FOLDER);
+      ipa.configure(global.fx.config);
     } catch (error) { }
   });
 
   it('should be empty and invalid', () => {
     session = new Session();
+
     expect(session.token).to.be.empty;
     expect(session.isValid()).to.be.false;
   });
 
   it('should have token not empty', () => {
     session = new Session(FAKE_TOKEN);
-    expect(session.token).not.to.be.empty;
+    expect(session.token).to.not.be.empty;
   });
 
-  it('should have an valid token', () => {
+  it('should use 4.4 prior validation - invalid', () => {
     session = new Session(FAKE_TOKEN);
     expect(session.isValid()).to.be.false;
   });
 
-  it('should use 4.5 validation', () => {
+  it('should use 4.4 prior validation - valid', () => {
+    session = new Session(FUTURE_FAKE_TOKEN);
+    expect(session.isValid()).to.be.true;
+  });
+
+  it('should use 4.5 validation - valid', () => {
     session = new Session(FAKE_TOKEN45);
     expect(session.isValid()).to.be.true;
   });
 
-  // Cached token is not working anymore since versin 4.5 +
-  it('should use cache token', () => {
-    session = new Session(FAKE_TOKEN);
-    expect(session.token).not.to.be.empty;
-  });
-
-  it('should create cache folder', () => {
+  it('should create cache', () => {
+    expect(fs.existsSync(CACHE_FOLDER)).to.be.false;
     session = new Session(FAKE_TOKEN);
     expect(fs.existsSync(CACHE_FOLDER)).to.be.true;
+    expect(fs.existsSync(CACHE_PATH)).to.be.true;
+  });
+
+  it('should use cache token', () => {
+    session = new Session(FAKE_TOKEN);
+    session2 = new Session();
+    expect(session2.token).to.not.be.empty;
   });
 });
